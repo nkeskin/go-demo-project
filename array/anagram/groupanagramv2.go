1ebeea09a5bb90066e4d261bd394a14a3059de28
nkeskin <keskin.nmn@gmail.com>
2023-07-30 14:58:54 +0300

    add a v2 implementation for finding group anagrams which has better time complexity

--- array/anagram/groupanagramv2.go
+++ array/anagram/groupanagramv2.go
@@ -1,21 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // TODO add an implementation with lower time complexity
 func main() {
-	for _, v := range groupAnagramsV2([]string{"eat", "tea", "tan", "ate", "nat", "bat"}) {
+	for _, v := range groupAnagramsV2([]string{"c", "c"}) {
 		fmt.Println(v)
 	}
 }
 
 func groupAnagramsV2(strs []string) [][]string {
 	var group [][]string
-	for _, v := range strs {
-		for _, z := range v {
-			fmt.Println(z - 'a')
+	charMap := map[[26]int][]string{}
+	for i, v := range strs {
+		chars := [26]int{}
+		for _, c := range v {
+			val := chars[c-'a']
+			val++
+			chars[c-'a'] = val
 		}
-		fmt.Println()
+		val, ok := charMap[chars]
+		if ok {
+			val = append(val, v)
+		} else {
+			val = append(val, strs[i])
+		}
+		charMap[chars] = val
+		//fmt.Println(charMap[v])
+	}
+	for _, val := range charMap {
+		group = append(group, val)
 	}
 	return group
 }
