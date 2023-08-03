9afbbe2084b9ff2c8c96bcd75d6f0074f2d27f85
nkeskin <keskin.nmn@gmail.com>
2023-08-03 20:57:56 +0300

    add a working implementation

--- array/longestconsecutivesequence/longestconsecutivesequence.go
+++ array/longestconsecutivesequence/longestconsecutivesequence.go
@@ -2,10 +2,9 @@ package main
 
 import "fmt"
 
-// TODO Not working -- fix implementation
 func main() {
-	input := []int{1, 0, -1}
-	fmt.Println(longestConsecutive(input))
+	input := []int{9, 1, -3, 2, 4, 8, 3, -1, 6, -2, -4, 7}
+	fmt.Println(longestConsecutive2(input))
 }
 
 func longestConsecutive(nums []int) int {
@@ -27,6 +26,33 @@ func longestConsecutive(nums []int) int {
 		} else {
 			init = nums[i+1] + 1
 			temp = 1
+		}
+		if temp > max {
+			max = temp
+		}
+	}
+	return max
+}
+
+func longestConsecutive2(nums []int) int {
+	numMap := map[int]int{}
+	max := 0
+
+	for i := range nums {
+		numMap[nums[i]] = nums[i]
+	}
+	for i := range nums {
+		temp := 1
+		_, ok := numMap[nums[i]-1]
+		if !ok {
+			seq := 1
+			_, ok2 := numMap[nums[i]+seq]
+			for ok2 {
+				fmt.Println(nums[i] + seq)
+				seq++
+				temp++
+				_, ok2 = numMap[nums[i]+seq]
+			}
 		}
 		if temp > max {
 			max = temp
